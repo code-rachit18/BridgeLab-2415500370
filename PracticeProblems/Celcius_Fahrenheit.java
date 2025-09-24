991617aa91faf6fe0898fc207020f826029a8b99
code-rachit18 <rachitricky777@gmail.com>
2025-09-24 13:38:43 +0530

    Delete PracticeProblems directory

--- PracticeProblems/Celcius_Fahrenheit.java
+++
@@ -1,7 +0,0 @@
-class Celcius_Fahrenheit {
-    public static void main(String[] args) {
-        double celsius = 25.0;
-        double fahrenheit = (celsius * 9/5) + 32; 
-        System.out.println(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
-    }
-}
--- PracticeProblems/Power_calc.java
+++
@@ -1,8 +0,0 @@
-class Power_calc {
-    public static void main(String[] args) {
-        double base = 2.0;
-        double exponent = 3.0;
-        double result = Math.pow(base, exponent);
-        System.out.println("The result of " + base + " raised to the power of " + exponent + " is: " + result);
-    }
-}
