320d391e9b15b0d93f8b07bb4a1d82951084e5f0
code-rachit18 <rachitricky777@gmail.com>
2025-09-24 13:40:22 +0530

    Delete PracticeProblems directory

--- PracticeProblems/Volume__of_cylinder.java
+++
@@ -1,8 +0,0 @@
-class Volume__of_cylinder {
-    public static void main(String[] args) {
-        double radius = 5.0;
-        double height = 10.0;
-        double volume = Math.PI * radius * radius * height;
-        System.out.println("Volume of the cylinder with radius " + radius + " and height " + height + " is: " + volume);
-    }
-}
