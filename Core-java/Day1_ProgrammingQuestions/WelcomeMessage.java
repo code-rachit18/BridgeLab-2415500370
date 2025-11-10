70e2c7d89419576c253da947a1ccef8d8413253a
Rachit Dubey <rachitricky777@gmail.com>
2025-11-10 12:59:15 +0530

    Keep only OOPs content in OOPs-java branch

--- Core-java/Day1_ProgrammingQuestions/WelcomeMessage.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class WelcomeMessage {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Bridgelabz");
-        sc.close();
-    }
-}
--- Core-java/Day3_ProgrammingQuestions/02 Java Programming Elements Level 1 Lab Practice Solution/AgeOfHarry.java
+++
@@ -1,8 +0,0 @@
-// Write a program to find the age of Harry if the birth year is 2000. Assume the Current Year is 2024
-class AgeOfHarry {
-    public static void main(String[] args) {
-        int birthYear = 2000, currentYear = 2024;
-        int age = currentYear - birthYear;
-        System.out.println("Harry's age in " + currentYear + " is " + age);
-    }
-}
